a1c408127268ae860a422d9257591e7ebe0dc128
99hyeon <hyeon99@ajou.ac.kr>
2024-12-20 16:23:58 +0900

    git 연결

---
+++ src/main/java/com/example/pickly/PicklyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicklyApplication.class, args);
+	}
+
+}
